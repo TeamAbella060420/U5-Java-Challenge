18b345dc1444ce42bb6bb243253d8d82b20580da
TeamAbella060420 <jabella@rscm.com.sa>
2022-03-28 15:23:30 +0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -3,12 +3,15 @@
    - When quiz begins, no answers are correct
 */
 
+let correct = 0;
 
 // 2. Store the rank of a player
 
+let rank;
 
 // 3. Select the <main> HTML element
 
+const main = document.querySelector('main')
 
 /*
   4. Ask at least 5 questions
@@ -16,6 +19,30 @@
    - Keep track of the number of correct answers
 */
 
+const answer1 = prompt("Name a programming language that's also a gem.");
+if ( answer1.toUpperCase() === 'Ruby') {
+  coreect += 1;
+}
+
+const answer2 = prompt("Name a programming language that's also a snake.");
+if ( answer2.toUpperCase() === 'Phyton') {
+  coreect += 1;
+}
+
+const answer3 = prompt("What languages do you use to style web pages?");
+if ( answer3.toUpperCase() === 'CSS') {
+  coreect += 1;
+}
+
+const answer4 = prompt("What languages do you use to build the structure of the web pages?");
+if ( answer4.toUpperCase() === 'HTML') {
+  coreect += 1;
+}
+
+const answer5 = prompt("What languages do you use to build the structure of the web pages?");
+if ( answer5.toUpperCase() === 'HTML') {
+  coreect += 1;
+}
 
 /*
   5. Rank player based on number of correct answers
