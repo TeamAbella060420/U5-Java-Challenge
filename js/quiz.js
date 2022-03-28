f09331a5ac076cca1c49fdd9260acabe6b8b9266
TeamAbella060420 <jabella@rscm.com.sa>
2022-03-28 15:26:45 +0300

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,47 +1,39 @@
-/* 
+/*
   1. Store correct answers
    - When quiz begins, no answers are correct
 */
-
 let correct = 0;
 
 // 2. Store the rank of a player
-
 let rank;
 
 // 3. Select the <main> HTML element
-
-const main = document.querySelector('main')
+const main = document.querySelector('main');
 
 /*
   4. Ask at least 5 questions
    - Store each answer in a variable
    - Keep track of the number of correct answers
 */
-
 const answer1 = prompt("Name a programming language that's also a gem.");
-if ( answer1.toUpperCase() === 'Ruby') {
-  coreect += 1;
+if ( answer1.toUpperCase() === 'RUBY' ) {
+  correct += 1;
 }
-
 const answer2 = prompt("Name a programming language that's also a snake.");
-if ( answer2.toUpperCase() === 'Phyton') {
-  coreect += 1;
+if ( answer2.toUpperCase() === 'PYTHON' ) {
+  correct += 1;
 }
-
-const answer3 = prompt("What languages do you use to style web pages?");
-if ( answer3.toUpperCase() === 'CSS') {
-  coreect += 1;
+const answer3 = prompt("What language do you use to style web pages?");
+if ( answer3.toUpperCase() === 'CSS' ) {
+  correct += 1;
 }
-
-const answer4 = prompt("What languages do you use to build the structure of the web pages?");
-if ( answer4.toUpperCase() === 'HTML') {
-  coreect += 1;
+const answer4 = prompt("What language do you use to build the structure of web pages?");
+if ( answer4.toUpperCase() === 'HTML' ) {
+  correct += 1;
 }
-
-const answer5 = prompt("What languages do you use to build the structure of the web pages?");
-if ( answer5.toUpperCase() === 'HTML') {
-  coreect += 1;
+const answer5 = prompt("What language do you use to add interactivity to a web page?");
+if ( answer5.toUpperCase() === 'JAVASCRIPT' ) {
+  correct += 1;
 }
 
 /*
@@ -51,7 +43,18 @@ if ( answer5.toUpperCase() === 'HTML') {
    - 1-2 correct = Bronze
    - 0 correct = No crown
 */
-
+if ( correct === 5 ) {
+  rank = "Gold";
+} else if ( correct >= 3 ) {
+  rank = "Silver";
+} else if ( correct >= 1 ) {
+  rank = "Bronze";
+} else {
+  rank = "None :(";
+}
 
 // 6. Output results to the <main> element
-
+main.innerHTML = `
+  <h2>You got ${correct} out of 5 questions correct.</h2>
+  <p>Crown earned: <strong>${rank}</strong></p>
+`;
